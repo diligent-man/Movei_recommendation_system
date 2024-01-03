a0590649a010e9e9f9ff72cf01250f4c6b9a33bb
unknown <trongnd02@gmail.com>
2024-01-03 22:18:29 +0700

    add merge file

--- data_crawling.py
+++ data_crawling.py
@@ -6,23 +6,34 @@ import pandas as pd
 import requests as rq
 from tqdm import tqdm
 from pprint import pprint as pp
+import multiprocessing as mp
 from multiprocessing.pool import Pool
 
 from typing import List
 
 
 class Crawler:
-    def __init__(self, file_name: str, start_year: int, end_year: int,
+    def __init__(self, data_path: str, file_name: str, start_year: int, end_year: int,
                  headers: dict, url: str, lang: str, process_counter: int) -> None:
+        self.__data_path = data_path
         self.__file_name = file_name
         self.__start_year = start_year
         self.__end_year = end_year
-        self.__headers = headers
+
         self.__url = url
         self.__lang = lang
+        self.__headers = headers
         self.__process_counter = process_counter
 
     # Set-Get
+    @property
+    def data_path(self):
+        return self.__data_path
+
+    @data_path.setter
+    def data_path(self, value):
+        self.__data_path = value
+
     @property
     def start_year(self):
         return self.__start_year
@@ -79,6 +90,7 @@ class Crawler:
     def process_counter(self, process_counter: int):
         self.__process_counter = process_counter
 
+
     # Support methods
     def __GetTotalPages(self, year) -> int:
         # Get total pages for each year
@@ -94,74 +106,59 @@ class Crawler:
         fields = rq.get(url, headers=self.__headers).json()["results"][0].keys()
         return fields
 
-    def __FieldsWriting(self, year: int = 1920) -> None:
-        """
-        Args:
-            year:
-
-        Returns: Fields for writing at the beginning of csv file
-
-        """
-        # Write header to output file
-        fields = self.__GetFields(year)
-        df = pd.DataFrame(columns=fields)
-        df.to_csv(self.__file_name, mode='w', index=False)
-        return None
-
-
     # Main Methods
     def MetadataCrawler(self):
-        if options.file_extension == "csv":
-            self.__FieldsWriting()
+        """
+        Returns: {obj1},
+                 {obj2},
+                 {obj3}
 
-        # Start crawling
-        # JSON format: [
-        #               {obj1},
-        #               {obj2},
-        #               {obj3}
-        #               ]
-        # Don't forget to add "[" and "]" at the beginning and end of file after crawling procedure finished
+        Appropriate JSON format: [{obj1},
+                                  {obj2},
+                                  {obj3}]
+        => Don't forget to add "[" and "]" at the beginning and end of file after crawling procedure finished
+        """
+        # write each process into separate file and merge 'em later
+        file_prefix = mp.current_process().name.split("-")[1]
+        file_name = os.path.join(options.data_path, "raw_data", f"{file_prefix}_{self.__file_name}.{options.file_extension}")
+
+        # start crawling
         for year in tqdm(range(self.__start_year, self.__end_year), position=self.__process_counter,
                          desc=f"{self.__start_year} to {self.__end_year}", colour='white'):
             total_pages = self.__GetTotalPages(year)
-            # print(f"total pages: {total_pages}, year: {year}") # Tracking info
 
-            # Crawl page by page - 1 page comprises 20 results
+            # Crawl page by page -- 1 page comprises 20 results
             for page in range(1, total_pages + 1):
                 url = self.__url + self.__lang + "year=" + str(year) + "&" + "page=" + str(page)
-                response = rq.get(url, headers=self.__headers)
+                response = rq.get(url, headers=options.headers)
 
                 # Test "results" field is available or not
                 try:
                     result: list = response.json()["results"]
                     result: List[str] = [json.dumps(json_object, indent=4) for json_object in result]
                     # save to file
-                    if options.file_extension == "json" and len(result) != 0:
-                        with open(file=self.__file_name, mode="a", encoding="UTF-8") as f:
+                    if len(result) != 0:
+                        with open(file=file_name, mode="a", encoding="UTF-8") as f:
                             for json_object in result:
                                 f.write(json_object)
                                 f.write(",")
                                 f.write("\n")
-
-                    elif options.file_extension == "csv":
-                        pd.DataFrame(result).to_csv(self.__file_name, mode='a', header=False, index=False)
                 except Exception as e:
                     print(e)
         return None
 
 
-def execute_metadata_crawling(file_name: str, start_year: int, end_year: int,
+def execute_metadata_crawling(data_path:str, file_name: str, start_year: int, end_year: int,
                               headers: dict, url: str, lang: str, process_counter) -> None:
-    crawler = Crawler(file_name, start_year, end_year, headers, url, lang, process_counter)
+    crawler = Crawler(data_path, file_name, start_year, end_year, headers, url, lang, process_counter)
     crawler.MetadataCrawler()
+
     return None
 
 
 def set_up_metadata_crawling() -> None:
     global options
-
     pool = Pool(processes=options.num_of_processes)
-    options.file_name = os.path.join(options.save_path, f"{options.file_name}.{options.file_extension}")
 
     process_counter = 0
     lower_bound = options.start_year
@@ -171,14 +168,14 @@ def set_up_metadata_crawling() -> None:
     configurations = []
     while lower_bound + interval < options.end_year:
         # remaining years will be handled after this while loop
-        configurations.append((options.file_name, lower_bound, lower_bound + interval,
+        configurations.append((options.data_path, options.file_name, lower_bound, lower_bound + interval,
                                options.headers, options.url, options.lang, process_counter))
         # Update
         lower_bound += interval
         process_counter += 1
 
     # Handle remaining years
-    configurations.append((options.file_name, lower_bound, options.end_year,
+    configurations.append((options.data_path, options.file_name, lower_bound, options.end_year,
                            options.headers, options.url, options.lang, process_counter))
 
     # creates multiprocesses in pool
@@ -217,24 +214,45 @@ parser.add_argument("--headers", type=dict, default={"accept": "application/json
 parser.add_argument("--start_year", type=int, default=1920)
 parser.add_argument("--end_year", type=int, default=2024)
 parser.add_argument("--num_of_processes", type=int, default=25)
-parser.add_argument("--save_path", type=str, default=os.path.join(os.getcwd(), "data", "raw_data"))
+parser.add_argument("--data_path", type=str, default=os.path.join(os.getcwd(), "data"))
 parser.add_argument("--file_extension", type=str, default="json")
-# key, url, file_name: depend on what will be crawled
-parser.add_argument("--key", type=str)
+# url, file_name: depend on what will be crawled
 parser.add_argument("--url", type=str)
 parser.add_argument("--file_name", type=str)
 options = parser.parse_args()
 
 
+def merge_tmp_file():
+    global options
+
+    path = os.path.join(options.data_path, "raw_data")
+    file_lst = os.listdir(path)
+
+    # remove metadata.json if it exists
+    if options.file_name in file_lst:
+        file_lst = file_lst.remove(options.file_name)
+        os.remove(os.path.join(path, options.file_name))
+
+    with open(file=os.path.join(path, options.file_name), mode="w", encoding="UTF-8") as writer:
+        for file in file_lst:
+            with open(file=os.path.join(path, file), mode="r", encoding="UTF-8") as reader:
+                data = json.loads(reader.read())
+                data = pd.json_normalize(data)
+                # writer.write(json.dumps(data, indent=4))
 def main() -> None:
+
+    # Metadata crawling
     options.url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&"
     options.file_name = "metadata"
     set_up_metadata_crawling()
+    # merge_tmp_file()
 
-    metadata_path = os.path.join(options.save_path, f"{options.file_name}.{options.file_extension}")
-    options.url = "https://api.themoviedb.org/3/movie/"
-    options.file_name = "movie_detail"
-    set_up_movie_detail_crawling(metadata_path)
+
+    # Movie detail crawling
+    # metadata_path = os.path.join(options.save_path, f"{options.file_name}.{options.file_extension}")
+    # options.url = "https://api.themoviedb.org/3/movie/"
+    # options.file_name = "movie_detail"
+    # set_up_movie_detail_crawling(metadata_path)
     return None
 
 
